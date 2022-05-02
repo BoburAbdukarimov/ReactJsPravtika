6d703c354d143350e53ef6c8ef4b178abdc92b55
bobur02k@gmail.com <bobur02k@gmail.com>
2022-05-02 21:39:16 +0500

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './style.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './components/Home';
@@ -21,11 +21,6 @@ function App() {
   </Routes>
   <Footer/>
 </Router>
-
-
-    // <div className="app">
-    //   <button onClick={googleSignInWithGoogle} className='btn btn-primary'>google</button>
-    // </div>
   );
 };
 
